e82b51b2a1f03d3521d049c2eac2818913a1c7a2
Euller Batista <eullersb@hotmail.com>
2026-09-25 03:29:01 -0300

    Adicionar log de erros no player

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const prism = require('prism-media');
-const ffmpeg = require('ffmpeg-static');
+if (message.content === '!mixfm') {
+  if (message.member.voice.channel) {
+    connection = joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-});
+    const player = createAudioPlayer();
 
-// Link da rádio Mix FM (AAC)
-const RADIO_URL = "https://playerservices.streamtheworld.com/api/livestream-redirect/MIXFM_SAOPAULOAAC.aac";
+    // Adiciona log de erros
+    player.on('error', error => {
+      console.error('Erro no player:', error.message, error.stack);
+    });
 
-// variável para guardar a conexão
-let connection;
+    const ffmpegStream = new prism.FFmpeg({
+      args: [
+        '-reconnect', '1',
+        '-reconnect_streamed', '1',
+        '-reconnect_delay_max', '5',
+        '-i', RADIO_URL,
+        '-c:a', 'libopus',   // força codec Opus
+        '-f', 'opus',
+        '-ar', '48000',
+        '-ac', '2'
+      ],
+      shell: false,
+      ffmpegPath: ffmpeg
+    });
 
-client.on('messageCreate', async message => {
-  if (message.content === '!mixfm') {
-    if (message.member.voice.channel) {
-      connection = joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      });
+    const resource = createAudioResource(ffmpegStream, { inputType: 'opus' });
+    player.play(resource);
+    connection.subscribe(player);
 
-      const player = createAudioPlayer();
-
-const ffmpegStream = new prism.FFmpeg({
-  args: [
-    '-reconnect', '1',
-    '-reconnect_streamed', '1',
-    '-reconnect_delay_max', '5',
-    '-i', RADIO_URL,
-    '-f', 'opus',        // saída em Opus
-    '-ar', '48000',      // taxa de amostragem
-    '-ac', '2'           // estéreo
-  ],
-  shell: false,
-  ffmpegPath: ffmpeg
-});
-
-const resource = createAudioResource(ffmpegStream, { inputType: 'opus' });
-
-      player.play(resource);
-      connection.subscribe(player);
-
-      message.reply("🎶 Tocando Rádio Mix FM São Paulo!");
-    } else {
-      message.reply("Você precisa estar em um canal de voz!");
-    }
+    message.reply("Tocando Rádio Mix FM São Paulo!");
+  } else {
+    message.reply("Você precisa estar em um canal de voz!");
   }
-
-  if (message.content === '!stop') {
-    if (connection) {
-      connection.destroy();
-      connection = null; // limpa a referência
-      message.reply("🛑 Rádio Mix FM desligada!");
-    } else {
-      message.reply("O bot não está em nenhum canal de voz.");
-    }
-  }
-});
-
-client.login(process.env.TOKEN);
+}
