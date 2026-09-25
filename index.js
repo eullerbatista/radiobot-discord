1474dc4d1302d99ab645b0da36350132bfdabefa
Euller Batista <eullersb@hotmail.com>
2026-09-25 02:47:34 -0300

    Resolvido conflito em Index.Js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const prism = require('prism-media');
 const ffmpeg = require('ffmpeg-static');
 
@@ -15,10 +15,13 @@ const client = new Client({
 // Link da rádio Mix FM (AAC)
 const RADIO_URL = "https://playerservices.streamtheworld.com/api/livestream-redirect/MIXFM_SAOPAULOAAC.aac";
 
+// variável para guardar a conexão
+let connection;
+
 client.on('messageCreate', async message => {
   if (message.content === '!mixfm') {
     if (message.member.voice.channel) {
-      const connection = joinVoiceChannel({
+      connection = joinVoiceChannel({
         channelId: message.member.voice.channel.id,
         guildId: message.guild.id,
         adapterCreator: message.guild.voiceAdapterCreator,
@@ -26,7 +29,6 @@ client.on('messageCreate', async message => {
 
       const player = createAudioPlayer();
 
-      // Usa prism-media com ffmpeg-static para converter AAC em PCM
       const ffmpegStream = new prism.FFmpeg({
         args: [
           '-reconnect', '1',
@@ -45,18 +47,17 @@ client.on('messageCreate', async message => {
       player.play(resource);
       connection.subscribe(player);
 
-      message.reply("Tocando Rádio Mix FM São Paulo!");
+      message.reply("🎶 Tocando Rádio Mix FM São Paulo!");
     } else {
       message.reply("Você precisa estar em um canal de voz!");
     }
   }
 
-  // Novo comando !stop
   if (message.content === '!stop') {
-    const connection = getVoiceConnection(message.guild.id);
     if (connection) {
       connection.destroy();
-      message.reply("botZADA encerrado, digite !mixfm para reconectar!");
+      connection = null; // limpa a referência
+      message.reply("🛑 Rádio Mix FM desligada!");
     } else {
       message.reply("O bot não está em nenhum canal de voz.");
     }
